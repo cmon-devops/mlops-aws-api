85138e7d881d79dcf5e41eb846a35c1f408343e2
cmon-devops <rishmarun18@gmail.com>
2024-10-01 12:53:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+# app.py
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = joblib.load('model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    prediction = model.predict(np.array(data['input']).reshape(1, -1))
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
